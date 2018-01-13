fafe331f6b43c4e4d6d0de6fd73c7cdc2cce8015
Talita <talita.santos.castro@gmail.com>
2018-01-13 00:34:24 -0200

    Novo layout Cadastro de pessoa Juridica

---
+++ Smarket/app/src/main/java/com/example/talit/smarket/ActConsumidores/CadastroPessoaJuridica.java
@@ -0,0 +1,15 @@
+package com.example.talit.smarket.ActConsumidores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.talit.smarket.R;
+
+public class CadastroPessoaJuridica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_cadastro_pessoa_juridica);
+    }
+}
