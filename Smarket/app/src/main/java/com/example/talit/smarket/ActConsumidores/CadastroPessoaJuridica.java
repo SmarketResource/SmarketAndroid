d0b74c377321c75b6e6393e9181e326160264cc8
Talita <talita.santos.castro@gmail.com>
2018-01-16 01:27:18 -0200

    finalização das animações cadastro de PJ

--- Smarket/app/src/main/java/com/example/talit/smarket/ActConsumidores/CadastroPessoaJuridica.java
+++ Smarket/app/src/main/java/com/example/talit/smarket/ActConsumidores/CadastroPessoaJuridica.java
@@ -16,8 +16,10 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -34,25 +36,53 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
     private SimpleMaskFormatter smf;
     private MaskTextWatcher mtw2;
     private SimpleMaskFormatter smf2;
+    private MaskTextWatcher mtw3;
+    private SimpleMaskFormatter smf3;
     private LinearLayout dadosEmpresa;
     private LinearLayout dadosFuncionario;
     private LinearLayout dadosLogin;
     private RelativeLayout relativeLevelUm;
+    private RelativeLayout relativeLevelDois;
+    private RelativeLayout relativeLeveltres;
     private Animation direitaParaEsquerda;
     private Animation deBaixoParaCima;
     private EditText edtNomeFantasia;
     private EditText edtRazaoSocial;
     private EditText edtTelefone;
     private EditText edtCnpj;
+    private EditText edtNomeFuncionario;
+    private EditText edtSobrenome;
+    private EditText edtCpf;
+    private EditText edtEmailFunc;
+    private EditText edtSenha;
+    private EditText edtConfirmSenha;
     private boolean haNomeFantasia;
     private boolean haRazaoSocial;
     private boolean haTelefone;
     private boolean haCnpj;
+    private boolean haNomeFunc;
+    private boolean haSobrenomeFunc;
+    private boolean haCpfFunc;
+    private boolean haEmail;
+    private boolean haSenha;
+    private boolean haConfirmSenha;
+    private TextView txtLevelUm;
+    private TextView txtLevelDois;
     private TextView txtValidaNomeFantasia;
     private TextView txtRazaoSocial;
     private TextView txtValidaTelefone;
     private TextView txtValidadCnoj;
-    private ImageButton imgLevelDois;
+    private TextView txtValidadeNomeFun;
+    private TextView txtSobrenomeFunc;
+    private TextView txtValidaCpf;
+    private TextView txtValidaEmail;
+    private TextView txtValidaSenha;
+    private TextView txtValidaConfirmSenha;
+    private Button imgLevelDois;
+    private Button btnLevelTres;
+    private Button btnCadastrar;
+    private ImageView imageUm;
+    private ImageView imageDois;
 
     private static Context c;
     @Override
@@ -64,15 +94,35 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
         dadosFuncionario = findViewById(R.id.ln_dados_funcionarios);
         dadosLogin = findViewById(R.id.ln_dados_login);
         relativeLevelUm = findViewById(R.id.nivel_um);
+        relativeLevelDois = findViewById(R.id.nivel_dois);
+        relativeLeveltres = findViewById(R.id.nivel_tres);
         imgLevelDois = findViewById(R.id.bt_level_dois);
         edtNomeFantasia = findViewById(R.id.edt_nome_fantasia);
         edtRazaoSocial = findViewById(R.id.ed_razao_social);
         edtTelefone = findViewById(R.id.ed_telefones);
         edtCnpj = findViewById(R.id.ed_cnpj);
+        edtNomeFuncionario = findViewById(R.id.edt_nome_funcionario);
+        edtSobrenome = findViewById(R.id.edt_sobrenome_funcionario);
+        edtCpf = findViewById(R.id.ed_cpf_funcionario);
+        edtEmailFunc = findViewById(R.id.edt_user_funcionario);
+        edtSenha = findViewById(R.id.edt_senha_funcionario);
+        edtConfirmSenha = findViewById(R.id.edt_senha_confir);
+        txtLevelUm = findViewById(R.id.level_um_down);
+        txtLevelDois = findViewById(R.id.level_dois_down);
         txtValidaNomeFantasia = findViewById(R.id.txt_nome_fantasia);
         txtRazaoSocial = findViewById(R.id.txt_razao_social);
         txtValidaTelefone = findViewById(R.id.txt_telefone);
         txtValidadCnoj = findViewById(R.id.txt_cnpj);
+        txtValidadeNomeFun = findViewById(R.id.txt_nome_func);
+        txtSobrenomeFunc= findViewById(R.id.txt_sobrenome_func);
+        txtValidaCpf = findViewById(R.id.txt_cpf_func);
+        txtValidaEmail = findViewById(R.id.txt_email);
+        txtValidaSenha = findViewById(R.id.txt_senha);
+        txtValidaConfirmSenha = findViewById(R.id.txt_senha_confirm);
+        imageUm = findViewById(R.id.concluido_um);
+        imageDois = findViewById(R.id.concluido_dois);
+        btnLevelTres = findViewById(R.id.bt_level_tres);
+        btnCadastrar = findViewById(R.id.bt_cadastrar);
 
         direitaParaEsquerda = AnimationUtils.loadAnimation(this,R.anim.da_direita_para_esquerda);
         deBaixoParaCima = AnimationUtils.loadAnimation(this,R.anim.para_cima);
@@ -81,6 +131,12 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
         haRazaoSocial= false;
         haTelefone = false;
         haCnpj = false;
+        haNomeFunc = false;
+        haSobrenomeFunc = false;
+        haCpfFunc = false;
+        haEmail= false;
+        haSenha = false;
+        haConfirmSenha = false;
 
         smf = new SimpleMaskFormatter("(NN)NNNN-NNNN");
         mtw = new MaskTextWatcher(edtTelefone, smf);
@@ -89,6 +145,10 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
         smf2 = new SimpleMaskFormatter("NN.NNN.NNN/NNNN-NN");
         mtw2 = new MaskTextWatcher(edtCnpj, smf2);
         edtCnpj.addTextChangedListener(mtw2);
+
+        smf3 = new SimpleMaskFormatter("NNN.NNN.NNN-NN");
+        mtw3 = new MaskTextWatcher(edtCpf, smf3);
+        edtCpf.addTextChangedListener(mtw3);
 
         edtNomeFantasia.addTextChangedListener(new TextWatcher() {
             @Override
@@ -112,7 +172,7 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
                     txtValidaNomeFantasia.setText(null);
                     haNomeFantasia = false;
                 }
-                validaStepDois();
+                validaStepUm();
             }
 
             @Override
@@ -142,7 +202,7 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
                     txtRazaoSocial.setText(null);
                     haRazaoSocial = false;
                 }
-                validaStepDois();
+                validaStepUm();
             }
 
             @Override
@@ -172,7 +232,7 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
                     haTelefone = false;
                     txtValidaTelefone.setText(null);
                 }
-                validaStepDois();
+                validaStepUm();
             }
 
             @Override
@@ -190,7 +250,7 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
                 if (TextUtils.isEmpty(charSequence)) {
-                    txtValidadCnoj.setText(R.string.txt_telefone_invalido);
+                    txtValidadCnoj.setText(R.string.txt_campo_obrigatorio);
                     Validacoes.requestFocus(edtCnpj);
                     haCnpj = true;
 
@@ -203,7 +263,197 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
                     haCnpj = false;
                     txtValidadCnoj.setText(null);
                 }
+                validaStepUm();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        edtNomeFuncionario.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (TextUtils.isEmpty(charSequence)) {
+                    txtValidadeNomeFun.setText(R.string.txt_campo_obrigatorio);
+                    Validacoes.requestFocus(edtNomeFuncionario);
+                    haNomeFunc = true;
+
+                } else if (charSequence.length() > 150) {
+                    txtValidadeNomeFun.setText(R.string.txt_nome_grande);
+                    Validacoes.requestFocus(edtNomeFuncionario);
+                    haNomeFunc = true;
+
+                } else {
+                    txtValidadeNomeFun.setText(null);
+                    haNomeFunc = false;
+                }
                 validaStepDois();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        edtSobrenome.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (TextUtils.isEmpty(charSequence)) {
+                    txtSobrenomeFunc.setText(R.string.txt_campo_obrigatorio);
+                    Validacoes.requestFocus(edtSobrenome);
+                    haSobrenomeFunc = true;
+
+                } else if (charSequence.length() > 150) {
+                    txtSobrenomeFunc.setText(R.string.txt_nome_grande);
+                    Validacoes.requestFocus(edtSobrenome);
+                    haSobrenomeFunc = true;
+
+                } else {
+                    txtSobrenomeFunc.setText(null);
+                    haSobrenomeFunc = false;
+                }
+                validaStepDois();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        edtCpf.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (TextUtils.isEmpty(charSequence)) {
+                    txtValidaCpf.setText(R.string.txt_campo_obrigatorio);
+                    Validacoes.requestFocus(edtCpf);
+                    haCpfFunc = true;
+
+                } else if (edtCpf.getText().toString().length() != 14) {
+                    txtValidaCpf.setText(R.string.txt_valida_cpf);
+                    Validacoes.requestFocus(edtCpf);
+                    haCpfFunc = true;
+
+                } else {
+                    haCpfFunc = false;
+                    txtValidaCpf.setText(null);
+                }
+                validaStepDois();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        edtEmailFunc.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (TextUtils.isEmpty(edtEmailFunc.getText().toString())) {
+                    txtValidaEmail.setText(R.string.txt_campo_obrigatorio);
+                    Validacoes.requestFocus(edtEmailFunc);
+                    haEmail = true;
+
+                } else if (!Validacoes.validaEmail(edtEmailFunc.getText().toString())) {
+                    txtValidaEmail.setText(R.string.txt_email_dig_incorretamente);
+                    Validacoes.requestFocus(edtEmailFunc);
+                    haEmail = true;
+
+                } else {
+                    haEmail = false;
+                    txtValidaEmail.setText(null);
+                }
+                validaCadastro();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        edtSenha.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (TextUtils.isEmpty(edtSenha.getText().toString())) {
+                    txtValidaSenha.setText(R.string.txt_campo_obrigatorio);
+                    Validacoes.requestFocus(edtSenha);
+                    haSenha = true;
+
+                } else if (!Validacoes.validaSenha(edtSenha.getText().toString())) {
+                    txtValidaSenha.setText(R.string.txt_senha_pequena);
+                    Validacoes.requestFocus(edtSenha);
+                    haSenha = true;
+
+                } else {
+                    haSenha = false;
+                    txtValidaSenha.setText(null);
+                }
+                validaCadastro();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+        edtConfirmSenha.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                if (TextUtils.isEmpty(edtConfirmSenha.getText().toString())) {
+                    txtValidaConfirmSenha.setText(R.string.txt_campo_obrigatorio);
+                    Validacoes.requestFocus(edtConfirmSenha);
+                    haConfirmSenha = true;
+
+                } else if (edtConfirmSenha.length() <= 4) {
+                    txtValidaConfirmSenha.setText(R.string.txt_senha_pequena);
+                    Validacoes.requestFocus(edtConfirmSenha);
+                    haConfirmSenha = true;
+
+                } else if (!edtConfirmSenha.getText().toString().equals(edtSenha.getText().toString())) {
+                    txtValidaConfirmSenha.setText(R.string.txt_senha_identica);
+                    Validacoes.requestFocus(edtConfirmSenha);
+                    haConfirmSenha = true;
+
+                } else {
+                    haConfirmSenha = false;
+                    txtValidaConfirmSenha.setText(null);
+
+                }
+                validaCadastro();
             }
 
             @Override
@@ -215,26 +465,84 @@ public class CadastroPessoaJuridica extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 dadosEmpresa.setVisibility(View.GONE);
+                relativeLevelDois.setVisibility(View.GONE);
                 relativeLevelUm.setVisibility(View.VISIBLE);
                 dadosFuncionario.setVisibility(View.VISIBLE);
                 relativeLevelUm.setAnimation(deBaixoParaCima);
+                txtLevelUm.setText(null);
+                imageUm.setVisibility(View.VISIBLE);
                 dadosFuncionario.setAnimation(direitaParaEsquerda);
             }
         });
+        btnLevelTres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dadosFuncionario.setVisibility(View.GONE);
+                relativeLeveltres.setVisibility(View.GONE);
+                relativeLevelDois.setVisibility(View.VISIBLE);
+                dadosLogin.setVisibility(View.VISIBLE);
+                relativeLevelDois.setAnimation(deBaixoParaCima);
+                txtLevelDois.setText(null);
+                imageDois.setVisibility(View.VISIBLE);
+                dadosLogin.setAnimation(direitaParaEsquerda);
+            }
+        });
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
     }
+    public void validaCadastro(){
+        if(!TextUtils.isEmpty(edtEmailFunc.getText().toString())&& !TextUtils.isEmpty(edtSenha.getText().toString())&&
+                !TextUtils.isEmpty(edtConfirmSenha.getText().toString())){
+            if(!haEmail && !haSenha && !haConfirmSenha){
+                Validacoes.requestFocus(btnCadastrar);
+                btnCadastrar.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                btnCadastrar.setEnabled(true);
+            }else{
+                btnCadastrar.setBackgroundColor(getResources().getColor(R.color.colorAccentTransparent));
+                btnCadastrar.setEnabled(false);
+            }
 
+        }else{
+            btnCadastrar.setBackgroundColor(getResources().getColor(R.color.colorAccentTransparent));
+            btnCadastrar.setEnabled(false);
+        }
+    }
     public void validaStepDois(){
+        if(!TextUtils.isEmpty(edtNomeFuncionario.getText().toString()) && !TextUtils.isEmpty(edtSobrenome.getText().toString())&&
+                !TextUtils.isEmpty(edtCpf.getText().toString())){
+            if(!haNomeFunc && !haSobrenomeFunc && !haCpfFunc){
+                Validacoes.requestFocus(btnLevelTres);
+                btnLevelTres.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                btnLevelTres.setEnabled(true);
+            }else{
+                btnLevelTres.setBackgroundColor(getResources().getColor(R.color.colorAccentTransparent));
+                btnLevelTres.setEnabled(false);
+            }
+
+        }else{
+            btnLevelTres.setBackgroundColor(getResources().getColor(R.color.colorAccentTransparent));
+            btnLevelTres.setEnabled(false);
+        }
+    }
+    public void validaStepUm(){
         if(!TextUtils.isEmpty(edtNomeFantasia.getText().toString()) && !TextUtils.isEmpty(edtRazaoSocial.getText().toString()) &&
                 !TextUtils.isEmpty(edtTelefone.getText().toString())&& !TextUtils.isEmpty(edtCnpj.getText().toString())){
 
             if(!haNomeFantasia && !haRazaoSocial && !haTelefone && !haCnpj){
-               imgLevelDois.setVisibility(View.VISIBLE);
+                Validacoes.requestFocus(imgLevelDois);
+                imgLevelDois.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                imgLevelDois.setEnabled(true);
             }else{
-                imgLevelDois.setVisibility(View.INVISIBLE);
+                imgLevelDois.setBackgroundColor(getResources().getColor(R.color.colorAccentTransparent));
+                imgLevelDois.setEnabled(false);
             }
         }else{
-            imgLevelDois.setVisibility(View.INVISIBLE);
+            imgLevelDois.setBackgroundColor(getResources().getColor(R.color.colorAccentTransparent));
+            imgLevelDois.setEnabled(false);
         }
     }
     @Override
